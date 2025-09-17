6a09b5b86def69df10b5206ea98de8980685ace7
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-09-17 18:05:38 -0300

    Início de configuração para autenticação de usuários

---
+++ Backend/viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,17 @@
+package com.github.sugayamidori.viaseguraapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AuthorizationServerConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
