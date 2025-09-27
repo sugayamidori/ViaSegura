f8e5ac0643ad6d9209fd19dbaa82f9aad07fc2b1
Pedro Lapa <pedro.lapa1911@gmail.com>
2025-09-27 20:46:23 -0300

    Configurando o TokenProvider

--- Backend/viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/security/jwt/JwtTokenProvider.java
+++ Backend/viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/security/jwt/JwtTokenProvider.java
@@ -1,18 +1,33 @@
 package com.github.sugayamidori.viaseguraapi.security.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.github.sugayamidori.viaseguraapi.controller.dto.TokenDTO;
+import io.micrometer.core.instrument.config.validate.Validated;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
     @Value("security.jwt.token.secret-key")
-    private String secret;
+    private String secretKey;
 
     @Value("security.jwt.token.expire-length")
     private Long validityInMilliseconds;
@@ -23,5 +38,59 @@ public class JwtTokenProvider {
 
     @PostConstruct
     protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+
+    public TokenDTO createAccessToken(String email, List<String> roles) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        String accessToken = getAccessToken(email, roles, now, validity);
+        String refreshToken = getRefreshToken(email, roles, now);
+        return new TokenDTO(email, true, now, validity, accessToken, refreshToken);
+    }
+
+    private String getAccessToken(String email, List<String> roles, Date now, Date validity) {
+        String issueUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .withSubject(email)
+                .withIssuer(issueUrl)
+                .sign(algorithm);
+    }
+
+    private String getRefreshToken(String email, List<String> roles, Date now) {
+        Date refreshTokenValidity = new Date(now.getTime() + validityInMilliseconds);
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(refreshTokenValidity)
+                .withSubject(email)
+                .sign(algorithm);
+    }
+
+    public Authentication getAuthentication(String token) {
+        DecodedJWT decodedJWT = decodeToken(token);
+        UserDetails userDetails = this.userDetailsService
+                .loadUserByUsername(decodedJWT.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private DecodedJWT decodeToken(String token) {
+        Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+        JWTVerifier verifier = JWT.require(alg).build();
+        return verifier.verify(token);
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(StringUtils.isAllEmpty(bearerToken) && bearerToken.startsWith("Bearer ")) {
+
+        } else {
+            System.out.println("hv");
+        }
+        return "";
     }
 }
