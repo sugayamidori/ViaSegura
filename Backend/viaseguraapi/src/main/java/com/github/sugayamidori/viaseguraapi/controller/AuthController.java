12b9e2cd7dd8db814b75c0e77c9fa52e1a525bf7
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-09-29 15:16:51 -0300

    Adicionando Endpoint para autenticação do usuário

--- Backend/viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/controller/AuthController.java
+++ Backend/viaseguraapi/src/main/java/com/github/sugayamidori/viaseguraapi/controller/AuthController.java
@@ -1,13 +1,10 @@
 package com.github.sugayamidori.viaseguraapi.controller;
 
 import com.github.sugayamidori.viaseguraapi.controller.dto.LoginRequest;
-import com.github.sugayamidori.viaseguraapi.security.jwt.JwtTokenProvider;
+import com.github.sugayamidori.viaseguraapi.controller.dto.TokenDTO;
+import com.github.sugayamidori.viaseguraapi.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,22 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthService service;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.username(),
-                        loginRequest.password()
-                )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String token = jwtTokenProvider.createToken(authentication);
-
-        return ResponseEntity.ok(new JwtResponse(token));
+        TokenDTO tokenDTO = service.signIn(loginRequest);
+        return ResponseEntity.ok(tokenDTO);
     }
 }
